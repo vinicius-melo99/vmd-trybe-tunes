55681723b921f523fe0f5b9b817a65ac27d65416
Vinicius Melo <viniciusmelo317@gmail.com>
2024-09-25 14:12:59 -0300

    configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import { Routes } from 'react-router-dom';
+import Login from './pages/Login/Login';
+import Search from './pages/Search/Search';
+import Album from './pages/Album/Album';
+import Favorites from './pages/Favorites/Favorites';
+import ProfileEdit from './pages/ProfileEdit/ProfileEdit';
+import Profile from './pages/Profile/Profile';
+import NotFound from './pages/NotFound/NotFound';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  return <Routes></Routes>;
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="/album/:id" element={<Album />} />
+      <Route path="/favorites" element={<Favorites />} />
+      <Route path="/profile/edit" element={<ProfileEdit />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 }
 
 export default App;
